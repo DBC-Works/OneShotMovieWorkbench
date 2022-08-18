021d8131d880c6d848363c68a671213ac8e13210
DBC-Works <contact.d.b.c@gmail.com>
2022-08-18 21:04:41 +0900

    feat: add function to merge audio file into generated video
    close #2

--- FrameBinder.java
+++ FrameBinder.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Frame binder
@@ -9,6 +11,7 @@ final public class FrameBinder {
   private final String movieFileName;
   private final String path;
   private final String imgExt;
+  private final String soundFilePath;
   private final int rate;
 
   /**
@@ -17,12 +20,20 @@ final public class FrameBinder {
    * @param fileName   Movie file name to bind
    * @param targetPath Path where image files exist
    * @param ext        Extension of target image file name
+   * @param soundFile  Sound file path
    * @param frameRate  Movie file frame rate(per second)
    */
-  public FrameBinder(String fileName, String targetPath, String ext, int frameRate) {
+  public FrameBinder(
+    String fileName,
+    String targetPath,
+    String ext,
+    String soundFile,
+    int frameRate
+  ) {
     movieFileName = fileName;
     path = targetPath;
     imgExt = ext;
+    soundFilePath = soundFile;
     rate = frameRate;
   }
 
@@ -47,10 +58,43 @@ final public class FrameBinder {
     }
   }
 
+  private String[] createCommandLineParam() {
+    final ArrayList<String> command = new ArrayList<String>(Arrays.asList(
+      "ffmpeg",
+      "-framerate",
+      Integer.toString(rate),
+      "-i",
+      String.format("%%08d.%s", imgExt)
+    ));
+    if (soundFilePath != null) {
+      command.addAll(Arrays.asList("-i", soundFilePath));
+      final String[] compressOptions = {
+        "-codec:a",
+        "aac",
+        "-b:a",
+        "320k"
+      };
+      final String[] copyOptions =  {
+        "-codec:a",
+        "copy"
+      };
+      command.addAll(Arrays.asList(soundFilePath.toLowerCase().endsWith(".wav")
+        ? compressOptions
+        : copyOptions));
+    }
+    command.addAll(Arrays.asList(
+      "-pix_fmt",
+      "yuv420p",
+      "-r",
+      Integer.toString(rate),
+      movieFileName
+    ));
+
+    return command.toArray(new String[0]);
+  }
+
   private void execFfmpeg(File imgDir) throws IOException, InterruptedException {
-    final String[] cmd = { "ffmpeg", "-framerate", Integer.toString(rate), "-i", String.format("%%08d.%s", imgExt),
-        "-pix_fmt", "yuv420p", movieFileName };
-    final ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+    final ProcessBuilder processBuilder = new ProcessBuilder(createCommandLineParam());
     processBuilder.redirectErrorStream(true);
     processBuilder.inheritIO();
     processBuilder.directory(imgDir);
